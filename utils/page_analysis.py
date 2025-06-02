0a7c914a1a958c1d37442b7e289dac4ef3aa575b
vedr0 <teh_predator@hotmail.com>
2025-06-02 10:52:51 +0300

    Added logging.

--- utils/page_analysis.py
+++ utils/page_analysis.py
@@ -1,9 +1,16 @@
 import pytest
 import allure
 from playwright.sync_api import Page
+import logging
 
+
+logger = logging.getLogger(__name__)
 @allure.step("Анализ страницы результата поиска")
 def analyze_result_page(page: Page, test_case: dict, mode: str = "f1"):
+    if "showcaptcha" in page.url:
+        logger.info("📝 Появилась captcha — ждём редирект на страницу результата.")
+        page.wait_for_url(lambda url: "showcaptcha" not in url, timeout=30000)
+
     try:
         # Частный случай: переход на список фильмов по стране — только для f1
         if mode == "f1":
@@ -19,7 +26,7 @@ def analyze_result_page(page: Page, test_case: dict, mode: str = "f1"):
             if has_only_country:
                 expected_url_part = f"/lists/m_act[country]/{country_value}/"
                 if expected_url_part in page.url:
-                    print(f"✅ Перешли на страницу фильмов по стране: {page.url}")
+                    logger.info(f"✅ Перешли на страницу фильмов по стране: {page.url}")
                     return
                 else:
                     pytest.xfail(f"❌ Ожидался редирект на {expected_url_part}, но URL: {page.url}")
@@ -30,21 +37,21 @@ def analyze_result_page(page: Page, test_case: dict, mode: str = "f1"):
 
         # "Ничего не найдено"
         if page.locator("h2.textorangebig", has_text="К сожалению, по вашему запросу ничего не найдено...").is_visible():
-            print("✅ Ничего не найдено")
+            logger.info("✅ Ничего не найдено")
             return
 
         # Переадресация на похожие фильмы
         if page.locator("p.header", has_text="Скорее всего, вы ищете:").is_visible():
-            print("✅ Переадресация на страницу похожих результатов")
+            logger.info("✅ Переадресация на страницу похожих результатов")
             return
 
         # Переадресация на страницу конкретного фильма
         if "/film/" in page.url:
-            print(f"✅ Найден один фильм: {page.url}")
+            logger.info(f"✅ Найден один фильм: {page.url}")
             return
 
         # Страница результатов
-        print(f"📝 Результат поиска: {page.url}")
+        logger.info(f"📝 Результат поиска: {page.url}")
 
     except Exception as e:
         pytest.fail(f"❌ Ошибка при анализе результата: {e}")
